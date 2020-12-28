3a8555313ec8387ec20d5e332ecb00736fbd2a18
tony <tony.aldon.adm@gmail.com>
2020-12-28 08:08:22 +0100

    Inside Emacs 6 (part 3): add source files
    * src/inside-emacs-6/part-3/org-meta-return.el:
    * src/inside-emacs-6/part-3/org-table.org:
    * src/inside-emacs-6/part-3/utils.el:
      New files.

---
+++ src/inside-emacs-6/part-3/org-meta-return.el
@@ -0,0 +1,16 @@
+;;; my-org-meta-return
+
+(defun my-org-meta-return (&optional arg)
+  "Insert a new heading or wrap a region in a table.
+Calls `org-insert-heading', `org-insert-item' or
+`org-table-wrap-region', depending on context."
+  (interactive "P")
+  (org-check-before-invisible-edit 'insert)
+  (or (run-hook-with-args-until-success 'org-metareturn-hook)
+      (if (org-at-table-p)
+					(org-table-wrap-region arg)
+				(call-interactively (cond (arg #'org-insert-heading)
+																	((org-in-item-p) #'org-insert-item)
+																	(t #'org-insert-heading))))))
+
+(define-key org-mode-map (kbd "<M-return>") 'my-org-meta-return)
