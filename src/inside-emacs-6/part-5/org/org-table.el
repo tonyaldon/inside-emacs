a4e9db54b42547a15bf31762553b185ab2cbf4d5
tony <tony.aldon.adm@gmail.com>
2020-12-29 12:56:04 +0100

    Inside Emacs 6 (part 5): add source files
    * src/inside-emacs-6/part-5/org-table.org:
    * src/inside-emacs-6/part-5/org/org-table.el:
    * src/inside-emacs-6/part-5/utils.el:
      New files.

---
+++ src/inside-emacs-6/part-5/org/org-table.el
@@ -0,0 +1,88 @@
+;;; part of org.el
+;; find this command in the original file `lisp/org/org-table.el'
+;; Emacs source repository
+;; $ git clone https://git.savannah.gnu.org/git/emacs.git
+
+(defun org-table-copy-down (n)
+  "Copy the value of the current field one row below.
+
+If the field at the cursor is empty, copy the content of the
+nearest non-empty field above.  With argument N, use the Nth
+non-empty field.
+
+If the current field is not empty, it is copied down to the next
+row, and the cursor is moved with it.  Therefore, repeating this
+command causes the column to be filled row-by-row.
+
+If the variable `org-table-copy-increment' is non-nil and the
+field is a number, a timestamp, or is either prefixed or suffixed
+with a number, it will be incremented while copying.  By default,
+increment by the difference between the value in the current
+field and the one in the field above, if any.  To increment using
+a fixed integer, set `org-table-copy-increment' to a number.  In
+the case of a timestamp, increment by days.
+
+However, when N is 0, do not increment the field at all."
+  (interactive "p")
+  (org-table-check-inside-data-field)
+  (let* ((beg (org-table-begin))
+				 (column (org-table-current-column))
+				 (initial-field (save-excursion
+													(let ((f (org-string-nw-p (org-table-get-field))))
+														(and f (org-trim f)))))
+				 field field-above next-field)
+    (save-excursion
+      ;; Get reference field.
+      (if initial-field (setq field initial-field)
+				(beginning-of-line)
+				(setq field
+							(catch :exit
+								(while (re-search-backward org-table-dataline-regexp beg t)
+									(let ((f (org-string-nw-p (org-table-get-field column))))
+										(cond ((and (> n 1) f) (cl-decf n))
+													(f (throw :exit (org-trim f)))
+													(t nil))
+										(beginning-of-line)))
+								(user-error "No non-empty field found"))))
+      ;; Check if increment is appropriate, and how it should be done.
+      (when (and org-table-copy-increment (/= n 0))
+				;; If increment step is not explicit, get non-empty field just
+				;; above the field being incremented to guess it.
+				(unless (numberp org-table-copy-increment)
+					(setq field-above
+								(let ((f (unless (= beg (line-beginning-position))
+													 (forward-line -1)
+													 (not (org-at-table-hline-p))
+													 (org-table-get-field column))))
+									(and (org-string-nw-p f)
+											 (org-trim f)))))
+				;; Compute next field.
+				(setq next-field (org-table--increment-field field field-above))))
+    ;; Since initial field in not empty, we modify row below instead.
+    ;; Skip alignment since we do it at the end of the process anyway.
+    (when initial-field
+      (let ((org-table-may-need-update nil)) (org-table-next-row))
+      (org-table-blank-field))
+    ;; Insert the new field.  NEW-FIELD may be nil if
+    ;; `org-table-increment' is nil, or N = 0.  In that case, copy
+    ;; FIELD.
+    (insert (or next-field field))
+    (org-table-maybe-recalculate-line)
+    (org-table-align)))
+
+(defun org-table-blank-field ()
+  "Blank the current table field or active region."
+  (interactive)
+  (org-table-check-inside-data-field)
+  (if (and (called-interactively-p 'any) (org-region-active-p))
+      (let (org-table-clip)
+				(org-table-cut-region (region-beginning) (region-end)))
+    (skip-chars-backward "^|")
+    (backward-char 1)
+    (if (looking-at "|[^|\n]+")
+				(let* ((pos (match-beginning 0))
+							 (match (match-string 0))
+							 (len (org-string-width match)))
+					(replace-match (concat "|" (make-string (1- len) ?\ )))
+					(goto-char (+ 2 pos))
+					(substring match 1)))))
---
+++ src/inside-emacs-6/part-5/utils.el
@@ -0,0 +1,101 @@
+;;; ta-mark-inside-dwim
+
+(require 'expand-region)
+(require 'smartparens)
+
+(defun ta-mark-inside-org-table ()
+  "Mark current field inside org-table."
+  (interactive)
+  (when (org-at-table-p)
+    (push-mark (point))
+		(re-search-forward "|")
+		(backward-char)
+		(skip-chars-backward " ")
+		(push-mark (point))
+		(set-mark (point))
+		(org-table-beginning-of-field 1)))
+
+(defun ta-mark-inside-pairs ()
+  "An other way to do `er/mark-inside-pairs' but work for sgml-tag too."
+  (interactive)
+  (ta-goto-begining-of-string (point))
+  ;; todo: do thing when inside a tag <tag name="tony"> (maybe use the function sgml-begining-of-tag)
+  (sp-backward-up-sexp)
+  (sp-mark-sexp)
+  (sp-down-sexp)
+  (exchange-point-and-mark)
+  (sp-backward-down-sexp)
+  (exchange-point-and-mark))
+
+
+(defun ta-mark-inside-dwim (&optional arg)
+  "Mark things inside quotes if point is inside a string.
+
+If not inside string, mark inside table field in `org-mode'.
+In other modes, mark things inside pairs.
+If call two times consecutively mark inside pairs."
+  (interactive)
+  (cond ((equal last-command 'ta-mark-inside-dwim)
+				 (call-interactively 'ta-mark-inside-pairs))
+				((er--point-inside-string-p) ;FIXME: return nil inside string in markdown-mode
+				 (call-interactively 'er/mark-inside-quotes))
+				((and (equal major-mode 'org-mode) (org-at-table-p))
+				 (ta-mark-inside-org-table))
+				(t (call-interactively 'ta-mark-inside-pairs))))
+
+;;; ta-mark-sexp-at-point
+
+(require 'smartparens)
+
+(defun ta--point-at-beginnig-sp-sexp-p ()
+  "Return non-nil if `point' is at the beginning of a sp-sexp
+
+and :op non empty. See `sp-get-thing'."
+  (let ((ok (sp-get-thing)))
+    (when ok
+      (when (and (eq (point) (sp-get ok :beg))
+                 (not (string-empty-p (sp-get ok :op))))
+        (point)))))
+
+(defun ta--mark-sexp-at-point ()
+  "Mark the `sexp' at point."
+  (let ((sexp-beg (beginning-of-thing 'sexp))
+        (sexp-end (end-of-thing 'sexp)))
+    (goto-char sexp-end)
+    (set-mark sexp-end)
+    (goto-char sexp-beg)))
+
+(defun ta-mark-sexp-at-point ()
+  "Mark the `sexp' at point. See `sexp-at-point' and `sp-mark-sexp'
+(smartparens packages)."
+  (interactive)
+  (if (or (ta--point-at-beginnig-sp-sexp-p)
+          (eq (following-char) ?<))
+      (sp-mark-sexp)
+    (if (eq (preceding-char) ?\")
+        (progn
+          (sp-backward-sexp)
+          (sp-mark-sexp))
+      (if (and (memq (following-char) '(32 ?\) ?\] ?\} ?>))
+               (looking-back "[[:alnum:]]" 1))
+          (backward-char 1))
+      (ta--mark-sexp-at-point))))
+
+;;; ta-org-table-previous-row
+
+(defun ta-org-table-previous-row ()
+  "Go to the previous row (same column) in the current table.
+Before doing so, re-align the table if necessary."
+  (interactive)
+	(unless (org-at-table-hline-p)
+		(org-table-maybe-eval-formula)
+		(org-table-maybe-recalculate-line))
+  (if (and org-table-automatic-realign
+					 org-table-may-need-update)
+      (org-table-align))
+	(let ((col (org-table-current-column)))
+    (when (and (org-at-table-p)
+               (not (= (org-table-current-line) 1)))
+			(previous-line)
+      (unless (org-at-table-hline-p)
+			  (org-table-goto-column col)))))
